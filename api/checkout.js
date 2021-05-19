b1a0af352deb983c268b75f8d8090e81a756fbf5
Kenny Bass <kenny_bass@outlook.com>
2021-05-19 11:33:51 -0500

    shipping

--- api/checkout.js
+++ api/checkout.js
@@ -5,6 +5,10 @@ export default async (req, res) => {
   const session = await stripe.checkout.sessions.create({
     payment_method_types: ["card"],
     line_items: req.body.order,
+    shipping_rates: ["shr_1IssKGC61AYwWLSuua1KcYSU"],
+    shipping_address_collection: {
+      allowed_countries: ['US'],
+    },
     success_url: `${hostUrl}?session_id={CHECKOUT_SESSION_ID}`,
     cancel_url: `${hostUrl}?failed=true`
   });
