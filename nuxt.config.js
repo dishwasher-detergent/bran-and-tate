11aa76a2582d4f4961f986d781f60d95cff96777
Kenny Bass <kenny_bass@outlook.com>
2021-05-19 14:30:47 -0500

    transition

--- nuxt.config.js
+++ nuxt.config.js
@@ -44,7 +44,7 @@ export default {
 
     // Global CSS: https://go.nuxtjs.dev/config-css
     css: ['~/assets/main.css'],
-    layoutTransition: 'my-layouts',
+    pageTransition: 'my-layouts',
 
     // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
     plugins : [
