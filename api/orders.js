04d380777b1d8e9bdb832daac197b3d5b4922abc
Kenny Bass <kbass@fundsforlearning.com>
2021-05-21 15:53:45 -0500

    Update orders.js

--- api/orders.js
+++ api/orders.js
@@ -1,7 +1,9 @@
 const stripe = require("stripe")(process.env.NUXT_STRIPE_SECRET_KEY);
 
 export default async (req, res) => {
-	const orders = await stripe.orders.list({});
+	const orders = await stripe.orders.list({
+		limit: 100,
+	});
 
 	return res.status(200).json(orders)
 };
