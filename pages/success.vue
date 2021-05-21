7d7a92932a555f4fb5f5ddb6bb4a8fd7c02d0440
Kenny Bass <kbass@fundsforlearning.com>
2021-05-21 15:46:31 -0500

    Update success.vue

--- pages/success.vue
+++ pages/success.vue
@@ -1,9 +1,9 @@
 <template>
 <div>
 	<div class="mx-auto max-w-7xl px-5 h-full pt-10">
-		<h1 v-if="id" class="font-bold text-4xl md:text-9xl text-center">Thank you for shopping with us! We will email you as soon as we ship your product(s).</h1>
+		<h1 v-if="id" class="font-bold text-4xl md:text-6xl text-center">Thank you for shopping with us! We will email you as soon as we ship your product(s).</h1>
 		<div v-else>
-			<h1 class="font-bold text-4xl md:text-9xl text-center">It seems there was an issue with the checkout process.</h1>
+			<h1 class="font-bold text-4xl md:text-6xl text-center">It seems there was an issue with the checkout process.</h1>
 			<p>If you have any questions please email support@branandtate.com</p>
 		</div>
 	</div>
