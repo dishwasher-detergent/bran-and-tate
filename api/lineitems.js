0f334e4a49b0c3a877878f9075e31650fe41d3d8
Kenny Bass <kbass@fundsforlearning.com>
2021-05-21 13:34:07 -0500

    Update lineitems.js

--- api/lineitems.js
+++ api/lineitems.js
@@ -1,10 +1,18 @@
 const stripe = require("stripe")(process.env.NUXT_STRIPE_SECRET_KEY);
 
 export default async (req, res) => {
-	console.log(req.body.id)
 	const session = await stripe.checkout.sessions.retrieve(
 		req.body.id
 	);
 
-  return res.status(200).json(session);
+	if(session.payment_status == 'paid'){
+		stripe.checkout.sessions.listLineItems(
+			req.body.id,
+			function(err, lineItems) {
+				if(!err)
+					return res.status(200).json(lineItems);
+			}
+		);
+	}
+	
 };
