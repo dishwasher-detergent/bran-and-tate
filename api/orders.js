d0c9a92db1e5eb41bdd74312cdd94b0ea2dfc656
Kenny Bass <kbass@fundsforlearning.com>
2021-05-21 15:22:05 -0500

    Update orders.js

--- api/orders.js
+++ api/orders.js
@@ -2,7 +2,7 @@ const stripe = require("stripe")(process.env.NUXT_STRIPE_SECRET_KEY);
 
 export default async (req, res) => {
 	const session = await stripe.checkout.sessions.retrieve({
-		status_transitions: paid
+		status_transitions: "paid"
 	});
 
 	return res.status(200).json(session)
