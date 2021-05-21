a281aaa81eb7bf3569aaa4f45d6d15369db4f41a
Kenny Bass <kbass@fundsforlearning.com>
2021-05-21 14:56:44 -0500

    success

---
+++ pages/success.vue
@@ -0,0 +1,28 @@
+<template>
+<div>
+	<div class="mx-auto max-w-7xl px-5 h-full pt-10">
+		<h1>{{id}}</h1>
+	</div>
+</div>
+</template>
+<script>
+export default {
+	data(){
+		return{
+			id: null
+		}
+	},
+	async mounted(){
+		await this.retrieve_lineitems()
+	},
+	methods:{
+		async retrieve_lineitems() {
+			const { session_id = null } = this.$route.query;
+			const { data } = await this.$axios.post("/api/success", {
+				id: session_id
+			});
+			this.items = data
+	},
+	}
+}
+</script>
