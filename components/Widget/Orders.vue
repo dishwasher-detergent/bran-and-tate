e89e51d7d2696d0a51d23b1c1e2b78d2c3b58aef
Kenny Bass <kbass@fundsforlearning.com>
2021-05-21 15:19:31 -0500

    orders

--- components/Widget/Orders.vue
+++ components/Widget/Orders.vue
@@ -3,7 +3,25 @@
     <div
       class="p-4 ring-1 ring-gray-300 rounded-xl overflow-hidden bg-white w-full"
     >
-
+		{{data}}
     </div>
 </div>
 </template>
+<script>
+export default {
+	data(){
+		return{
+			data: null
+		}
+	},
+	async mounted(){
+		await this.retrieve_orders()
+	},
+	methods:{
+		async retrieve_orders() {
+			const { data } = await this.$axios.post("/api/orders");
+			this.data = data
+	},
+	}
+}
+</script>
