78166af93650e8075d028240e790469da305474c
Kenny Bass <kenny_bass@outlook.com>
2021-05-24 14:27:11 -0500

    beta

--- api/orders.js
+++ api/orders.js
@@ -1,8 +1,8 @@
 const stripe = require("stripe")(process.env.NUXT_STRIPE_SECRET_KEY);
 
 export default async (req, res) => {
-	const invoiceItems = await stripe.checkout.sessions.listLineItems(
-	req.body.id,
+	stripe.checkout.sessions.listLineItems(
+		req.body.id,
 	function(err, lineItems) {
 		return res.status(200).json(lineItems)
 	})
