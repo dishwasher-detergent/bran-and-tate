ae71ccdd3ba9959e5cb777ad8d2be8d2c98d919c
Kenny Bass <kenny_bass@outlook.com>
2021-05-25 16:46:28 -0500

    visuals

--- pages/index.vue
+++ pages/index.vue
@@ -7,43 +7,45 @@
           <h1 class="font-bold text-7xl md:text-9xl text-center">Bran & Tate Co.</h1>
         </div>
       </div>
-    </div>
-    <div class="sticky top-0 pt-6 z-50">
-      <div class="w-full md:h-16 ring-1 ring-gray-300 rounded-xl bg-white">
-        <div class="mx-auto max-w-7xl h-full grid grid-cols-3">
-          <div class="w-full col-span-2">
-            <NavSearch 
-              @sort="sorting"
-            />
-          </div>
-          <div
-            class="w-full flex flex-row justify-center md:justify-end space-between h-full md:pb-0 "
-          >
-          <NavSubItem type="drop">
-              <template v-slot:icon>
-                  <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 4a1 1 0 011-1h16a1 1 0 011 1v2.586a1 1 0 01-.293.707l-6.414 6.414a1 1 0 00-.293.707V17l-4 4v-6.586a1 1 0 00-.293-.707L3.293 7.293A1 1 0 013 6.586V4z" />
-                  </svg>
-              </template>
-              <template v-slot:content>
-                <div class="w-full h-full overflow-y-auto flex flex-col">
-                  <NavSubItem type="Porch" value="Porch" @sort="alter_filter">Porch</NavSubItem>
-                  <NavSubItem type="Wall" value="Wall" @sort="alter_filter">Wall</NavSubItem>
-                  <NavSubItem type="Seasonal" value="Seasonal" @sort="alter_filter">Seasonal</NavSubItem>
-                </div>
-              </template>
-            </NavSubItem>
-            <NavSubItem type="cart">
-              <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
-              </svg>
-              <div class="top-0 right-0 absolute px-1.5 h-5 flex items-center justify-center text-sm bg-red-500 text-white text-bold rounded-full ">{{$store.state.cart.length}}</div>
-            </NavSubItem>
-          </div>
+    </section>
+    <!-- Sub Nav Bar -->
+    <nav class="navbar mb-2 bg-white ring-1 ring-gray-300 text-neutral-content rounded-box sticky top-0 mt-6 px-3 z-50">
+      <div class="flex-1">
+        <div class="form-control w-full md:mr-8">
+          <input v-model="search" type="text" placeholder="Search" class="input input-bordered bg-gray-100 text-gray-900">
         </div>
-      </div>
-    </div>
-    <div class="w-full py-10">
+      </div> 
+      <div class="flex-none">
+        <div class="dropdown dropdown-hover dropdown-end">
+          <div tabindex="0" class="m-1 btn btn-square btn-ghost text-gray-900">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 4a1 1 0 011-1h16a1 1 0 011 1v2.586a1 1 0 01-.293.707l-6.414 6.414a1 1 0 00-.293.707V17l-4 4v-6.586a1 1 0 00-.293-.707L3.293 7.293A1 1 0 013 6.586V4z" />
+            </svg>  
+          </div> 
+          <ul class="ring-1 ring-gray-300 shadow menu dropdown-content bg-base-100 rounded-box w-52 text-gray-900 p-2">
+            <div class="form-control">
+              <label class="cursor-pointer label" v-for="type in types" :key="type.id">
+                <span class="label-text">{{type}}</span> 
+                <div>
+                  <input v-model="filter" type="checkbox" class="checkbox checkbox-primary" name="type" :value="type"> 
+                  <span class="checkbox-mark"></span>
+                </div>
+              </label>
+            </div>
+          </ul>
+        </div>
+      </div> 
+      <div class="flex-none">
+        <nuxt-link to="/cart" class="btn btn-square btn-ghost relative text-gray-900">
+          <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
+          </svg>
+          <div class="top-0 right-0 absolute px-1.5 h-5 flex items-center justify-center text-sm bg-red-500 text-white text-bold rounded-full ">{{$store.state.cart.length}}</div>
+        </nuxt-link>
+      </div> 
+    </nav>
+    <!-- Products -->
+    <section class="w-full py-10">
       <div
         class="max-w-7xl mx-auto grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 items-center justify-items-center gap-5"
       >
@@ -66,6 +68,7 @@ export default {
       products: [],
       search: '',
       filter: [],
+      types: []
     };
   },
   created(){
@@ -85,6 +88,7 @@ export default {
   },
   mounted(){
     this.getProducts()
+    this.getTypes()
   },
   methods:{
     async getProducts(){
@@ -97,16 +101,16 @@ export default {
           this.products = products
         }
     },
-    sorting(e){
-      this.search = e
-    },
-    alter_filter(e){
-      const index = this.filter.indexOf(e.target.value.toLowerCase())
-      if(index > -1){
-        this.filter.splice(index, 1)
-      } else {
-        this.filter.push(e.target.value.toLowerCase())
-      }
+    async getTypes(){
+      let { data: types, error } = await this.$supabase
+        .from('types')
+        .select('*')
+        .eq("title","product_types")
+        if(error){
+          console.error(error)
+        } else {
+          this.types = types[0].types
+        }
     },
     itemSearch(e){
       if(this.search.length > 0){
