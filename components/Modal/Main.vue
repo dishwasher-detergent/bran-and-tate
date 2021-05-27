3ecd7c54c0561beced8f5c07778d0c3b4b6dddaa
Kenny Bass <kenny_bass@outlook.com>
2021-05-27 15:50:08 -0500

    Update Main.vue

--- components/Modal/Main.vue
+++ components/Modal/Main.vue
@@ -63,7 +63,7 @@
 				class="w-full h-full pb-20 flex flex-col text-md md:text-xl relative"
 				>
 				<div class="w-full h-96 flex-none bg-gray-200 flex items-center justify-center">
-					<button aria-label="Close Modal" @click='close' class="h-6 w-6 absolute top-0 right-0 mt-2 mr-4 ring-1 ring-red-500 p-1 bg-red-200 text-red-500 rounded-full">
+					<button aria-label="Close Modal" @click='close' class="z-50 h-6 w-6 absolute top-0 right-0 mt-2 mr-4 ring-1 ring-red-500 p-1 bg-red-200 text-red-500 rounded-full">
 						<svg xmlns="http://www.w3.org/2000/svg" class="h-full w-full" viewBox="0 0 20 20" fill="currentColor">
 							<path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd" />
 						</svg>
