934c84a96c3b9b850ccf955ab9d4c323b54af16b
dishwasher-detergent <kenny_bass@outlook.com>
2021-05-31 19:55:29 -0500

    Update orders.js

--- api/orders.js
+++ api/orders.js
@@ -1,8 +1,8 @@
-const stripe = require("stripe")(process.env.NUXT_STRIPE_SECRET_KEY);
+const stripe = require('stripe')(process.env.NUXT_STRIPE_SECRET_KEY)
 
 export default async (req, res) => {
-	const orders = await stripe.orders.list({
-		limit: 3,
-	});
-	return res.status(200).json(orders)
-};
+  const invoices = await stripe.invoices.list({
+    limit: 3
+  })
+  return res.status(200).json(invoices)
+}
