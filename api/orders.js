d2586f9df0c86c8f994503ab0139b05accaf0ac3
dishwasher-detergent <kenny_bass@outlook.com>
2021-06-01 18:13:02 -0500

    Update orders.js

--- api/orders.js
+++ api/orders.js
@@ -1,11 +1,17 @@
 const stripe = require("stripe")(process.env.NUXT_STRIPE_SECRET_KEY);
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = process.env.NUXT_ENV_API_URL
+const supabaseKey = process.env.NUXT_ENV_API_KEY
+const supabase = createClient(supabaseUrl, supabaseKey)
+
 
 export default async (req, res) => {
 	const event = req.body
 	await stripe.checkout.sessions.listLineItems(
 		event.data.object.id,
 		async function(err, lineItems) {
-			const { data, error } =  await this.$supabase
+			const { data, error } =  await supabase
 			.from('orders')
 			.insert([
 				{ 
@@ -15,8 +21,9 @@ export default async (req, res) => {
 					line_items: lineItems
 				},
 			])
-			if(!error) return res.status(200).json({received: true})
-			else return res.status(200).json({received: error})
+
+			if(error) return res.status(200).json({received: error})
+			else return res.status(200).json({received: true})
 		}
 	)
 }
