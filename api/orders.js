53ebfe49f550b8fd96e6bfbab74c160ef8fd4791
Kenny Bass <kenny_bass@outlook.com>
2021-06-01 16:42:40 -0500

    Update orders.js

--- api/orders.js
+++ api/orders.js
@@ -2,28 +2,5 @@ const stripe = require("stripe")(process.env.NUXT_STRIPE_SECRET_KEY);
 
 export default async (req, res) => {
 	const event = req.body
-	const line;
-	await stripe.checkout.sessions.listLineItems(
-		event.data.object.id,
-		function(err, lineItems) {
-			console.log(lineItems)
-		}
-	);
 	return res.status(200).json({received: true})
-	// try{
-	// 	const { data, error } = await this.$supabase
-	// 	.from('orders')
-	// 	.insert([
-	// 		{ 
-	// 			order_id: event.data.object.id,
-	// 			shipping: event.data.object.shipping,
-	// 			payment_status: event.data.object.payment_status,
-	// 			line_items: line
-	// 		},
-	// 	])
-	// 	if(!error) return res.status(200).json({received: true})
-	// 	throw error
-	// } catch(error){
-	// 	return res.status(200).json({received: false})
-	// }
 }
