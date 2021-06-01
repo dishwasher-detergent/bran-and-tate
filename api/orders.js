59bdb08a643d4be772da9e8300d3a3492b2d413a
Kenny Bass <kenny_bass@outlook.com>
2021-06-01 16:05:23 -0500

    Update orders.js

--- api/orders.js
+++ api/orders.js
@@ -1,3 +1,18 @@
 export default async (req, res) => {
+	console.log(req)
 	return res.status(200).json({received: true})
+	
+	try{
+		const { data, error } = await this.$supabase
+		.from('orders')
+		.insert([
+			{ 
+				
+			},
+		])
+		if(!error) return res.status(200).json({received: true})
+		throw error
+	} catch(error){
+		return res.status(200).json({received: false})
+	}
 }
