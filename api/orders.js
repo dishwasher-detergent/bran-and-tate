2faa9ec7680ec95e06eb789a3a0923447db10cc6
Kenny Bass <kenny_bass@outlook.com>
2021-06-01 16:58:32 -0500

    Update orders.js

--- api/orders.js
+++ api/orders.js
@@ -2,29 +2,25 @@ const stripe = require("stripe")(process.env.NUXT_STRIPE_SECRET_KEY);
 
 export default async (req, res) => {
 	const event = req.body
-	let line;
 	await stripe.checkout.sessions.listLineItems(
 		event.data.object.id,
 		function(err, lineItems) {
 			try{
-                const { data, error } = await this.$supabase
-                .from('orders')
-                .insert([
-                    { 
-                        order_id: event.data.object.id, 
-                        description: this.description, 
-                        size: this.size,
-                        colors: this.colors,
-                        price: this.price,
-                        type: this.types
-                    },
-                ])
-                if(!error) return data
-                throw error
-            } catch(error){
-                return
-            }
+				await this.$supabase
+				.from('orders')
+				.insert([
+					{ 
+						order_id: event.data.object.id,
+						shipping: event.data.object.shipping,
+						payment_status: event.data.object.payment_status,
+						line_items: lineItems
+					},
+				])
+				if(!error) return res.status(200).json({received: true})
+				throw error
+			} catch(error){
+				return res.status(200).json({received: false})
+			}
 		}
 	)
-	return res.status(200).json({received: true})
 }
