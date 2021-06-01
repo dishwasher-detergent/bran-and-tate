8484cfe58102b30e40c4730322d80ec3aa9f93c2
Kenny Bass <kenny_bass@outlook.com>
2021-06-01 16:47:00 -0500

    Update orders.js

--- api/orders.js
+++ api/orders.js
@@ -2,5 +2,11 @@ const stripe = require("stripe")(process.env.NUXT_STRIPE_SECRET_KEY);
 
 export default async (req, res) => {
 	const event = req.body
+	stripe.checkout.sessions.listLineItems(
+		event.data.object.id,
+		function(err, lineItems) {
+			console.log(lineItems)
+		}
+	)
 	return res.status(200).json({received: true})
 }
