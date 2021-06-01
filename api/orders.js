330a9c994a8338bf0c6e86f863dd5336da4fd9a4
dishwasher-detergent <kenny_bass@outlook.com>
2021-05-31 20:54:28 -0500

    Update orders.js

--- api/orders.js
+++ api/orders.js
@@ -4,20 +4,25 @@ export default async (req, res) => {
   	const sessions = await stripe.checkout.sessions.list({})
 //   return res.status(200).json(sessions)
 	for(let i = 0; i < sessions.data.length; i++){
-		stripe.checkout.sessions.listLineItems(
-			'"' + sessions.data[i].id + '"',
-		function(err, lineItems) {
-			if(err) return res.status(200).json(err)
-			else return res.status(200).json(lineItems)
-			// order.push({
-			// 	information: {
-			// 		id: sessions.data[i].id,
-			// 		shipping: sessions.data[i].shipping,
-			// 		total: sessions.data[i].amount_total
-			// 	},
-			// 	items: lineItems.data
-			// })
-		});
+		let thing = await line_items(session.data[i])
+		return res.status(200).json(thing)
 	}
-	return res.status(200).json(sessions.data.length)
+	// return res.status(200).json(sessions.data.length)
+}
+
+async function line_items(session){
+	stripe.checkout.sessions.listLineItems(
+		session.id,
+	function(err, lineItems) {
+		if(err) return res.status(200).json(err)
+		else return res.status(200).json(lineItems)
+		// order.push({
+		// 	information: {
+		// 		id: sessions.data[i].id,
+		// 		shipping: sessions.data[i].shipping,
+		// 		total: sessions.data[i].amount_total
+		// 	},
+		// 	items: lineItems.data
+		// })
+	});
 }
