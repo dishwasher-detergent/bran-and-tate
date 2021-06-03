db65495d12f301f5936b45ab3a7542c3e7128878
dishwasher-detergent <kenny_bass@outlook.com>
2021-06-02 21:25:26 -0500

    Update vueimagekit.js

--- plugins/vueimagekit.js
+++ plugins/vueimagekit.js
@@ -1,8 +1,9 @@
 import Vue from 'vue'
 const {ImageKit} = require('imagekitio-vue')
+const hostUrl = process.env.NUXT_WEBSITE;
 
 Vue.use(ImageKit, {
   urlEndpoint: "https://ik.imagekit.io/branandtate/",
   publicKey: "public_3C3ZlEJz6tKgVa6Glcpb7YSQXe8=",
-  authenticationEndpoint: "https://branandtate.com/api/imageKit"
+  authenticationEndpoint: hostUrl + "/api/imageKit"
 })
