4c95a3ad16424d1f7d229d925f0c8029469f9d83
Kenny Bass <kbass@fundsforlearning.com>
2021-06-04 10:29:00 -0500

    Update imageKit.js

--- api/imageKit.js
+++ api/imageKit.js
@@ -1,16 +1,11 @@
-import ImageKit from "imagekitio-vue"
+ const ImageKit = require("imagekit");
 
 export default async (req, res) => {
-    const imagekit = await new ImageKit({
-        urlEndpoint: "https://ik.imagekit.io/branandtate/",
-        publicKey: "public_3C3ZlEJz6tKgVa6Glcpb7YSQXe8=",
-        privateKey: "private_qOOi2KOsyb01YAN68A8HjZ8vJxI="
+    const imagekit = new ImageKit({
+        urlEndpoint: "your_url_endpoint",
+        publicKey: "your_public_key",
+        privateKey: "your_privage_key"
     })
-
     console.log(imagekit)
-
-    var result = imagekit.getAuthenticationParameters();
-    console.log(result)
-
-    return res.status(200).json(result);
+    // return res.status(200).json(result);
 }
