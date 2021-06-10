69f928673b065f9073f33b8d5b16e1bac1d36ecc
Kenny Bass <kbass@fundsforlearning.com>
2021-06-10 13:43:26 -0500

    Update Sidebar.vue

--- components/Sidebar.vue
+++ components/Sidebar.vue
@@ -1,58 +1,142 @@
 <template>
-<div class="rounded-lg shadow bg-base-200 drawer drawer-mobile h-full">
-  <input id="my-drawer-2" type="checkbox" class="drawer-toggle"> 
-  <div class="drawer-content">
-    <nav class="border-b border-base-300 w-full h-16 bg-base-100 flex justify-between items-center px-4 lg:hidden sticky top-0 z-50">
-      <NavLogo/>
-      <label for="my-drawer-2" class="btn btn-square btn-md btn-ghost drawer-button lg:hidden">
-        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16m-7 6h7" />
-        </svg>
-      </label> 
-    </nav>
-    <slot></slot>
-  </div> 
-  <div class="drawer-side border-r border-base-300">
-    <label for="my-drawer-2" class="drawer-overlay"></label>
-    <ul class="menu p-4 w-80 bg-base-100  space-y-2">
-      <div class="w-full flex items-center justify-center">
-        <nuxt-link to="/">
-          <NavLogo />
-        </nuxt-link>
-      </div>
-      <Sidebar-item stage="Orders">
-        <span class="mr-4">
-          <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M9 16h.01" />
+  <div class="rounded-lg shadow bg-base-200 drawer drawer-mobile h-full">
+    <input id="my-drawer-2" type="checkbox" class="drawer-toggle" />
+    <div class="drawer-content">
+      <nav
+        class="
+          border-b border-base-300
+          w-full
+          h-16
+          bg-base-100
+          flex
+          justify-between
+          items-center
+          px-4
+          lg:hidden
+          sticky
+          top-0
+          z-50
+        "
+      >
+        <NavLogo />
+        <label for="my-drawer-2" class="btn btn-square btn-md btn-ghost drawer-button lg:hidden">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M4 6h16M4 12h16m-7 6h7"
+            />
           </svg>
-        </span>
-        Orders
-      </Sidebar-item>
-      <Sidebar-item stage="Product">
-        <span class="mr-4">
-          <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
-          </svg>
-        </span>
-        Add Product
-      </Sidebar-item>
-      <Sidebar-item stage="User">
-        <span class="mr-4">
-          <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M18 9v3m0 0v3m0-3h3m-3 0h-3m-2-5a4 4 0 11-8 0 4 4 0 018 0zM3 20a6 6 0 0112 0v1H3v-1z" />
-          </svg>
-        </span>
-        Add User
-      </Sidebar-item>
-      <Sidebar-item stage="Banner">
-        <span class="mr-4">
-          <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z" />
-          </svg>
-        </span>
-        Banners
-      </Sidebar-item>
-    </ul>
+        </label>
+      </nav>
+      <slot></slot>
+    </div>
+    <div class="drawer-side border-r border-base-300">
+      <label for="my-drawer-2" class="drawer-overlay"></label>
+      <ul class="menu p-4 w-80 bg-base-100 space-y-2">
+        <div class="w-full flex items-center justify-center">
+          <nuxt-link to="/">
+            <NavLogo />
+          </nuxt-link>
+        </div>
+        <section>
+          <li class="menu-title">
+            <span>Shop</span>
+          </li>
+          <div class="p-4 rounded-2xl bg-base-200">
+            <Sidebar-item stage="Orders">
+              <span class="mr-4">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="h-6 w-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M9 16h.01"
+                  />
+                </svg>
+              </span>
+              Orders
+            </Sidebar-item>
+            <Sidebar-item stage="Product">
+              <span class="mr-4">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="h-6 w-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
+                  />
+                </svg>
+              </span>
+              Add Product
+            </Sidebar-item>
+            <Sidebar-item stage="Banner">
+              <span class="mr-4">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="h-6 w-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z"
+                  />
+                </svg>
+              </span>
+              Banners
+            </Sidebar-item>
+          </div>
+        </section>
+        <section>
+          <li class="menu-title">
+            <span>User Management</span>
+          </li>
+          <div class="p-4 rounded-2xl bg-base-200">
+            <Sidebar-item stage="User">
+              <span class="mr-4">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="h-6 w-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M18 9v3m0 0v3m0-3h3m-3 0h-3m-2-5a4 4 0 11-8 0 4 4 0 018 0zM3 20a6 6 0 0112 0v1H3v-1z"
+                  />
+                </svg>
+              </span>
+              Add User
+            </Sidebar-item>
+          </div>
+        </section>
+      </ul>
+    </div>
   </div>
-</div>
 </template>
