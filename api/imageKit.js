b52c4aa518d18ba12700ec237907a5142aa0f9b0
dishwasher-detergent <33816169+dishwasher-detergent@users.noreply.github.com>
2021-07-13 20:20:27 -0500

    Update imageKit.js

--- api/imageKit.js
+++ api/imageKit.js
@@ -2,9 +2,9 @@ const ImageKit = require("imagekit");
 
 export default async (req, res) => {
     const imagekit = new ImageKit({
-        urlEndpoint: process.env.NUXT_IMAGEKIT_ENDPOINT,
-        publicKey: process.env.NUXT_IMAGEKIT_PUBLIC,
-        privateKey: process.env.NUXT_IMAGEKIT_PRIVATE
+        urlEndpoint: process.env.imageKit_endpoint,
+        publicKey: process.env.public_api,
+        privateKey: process.env.private_api
     })
     var result = imagekit.getAuthenticationParameters();
     return res.status(200).json(result);
