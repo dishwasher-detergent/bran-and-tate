48d19677029a69b4641c45db83796397cd6251ce
Kenneth Bass <kenny_bass@outlook.com>
2026-01-11 14:48:32 -0600

    fix grid

--- app/(app)/app/page.tsx
+++ app/(app)/app/page.tsx
@@ -45,7 +45,7 @@ export default async function Home() {
           </CardContent>
         </Card>
       ) : (
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
           {labels.map((label) => {
             return (
               <Card
